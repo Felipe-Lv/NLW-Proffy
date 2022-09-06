82914486ef7c1a6ad61382f4d158e662f55936f1
Felipe Alves <fbarros.silva2000@gmail.com>
2022-09-06 17:29:35 -0400

    continuação banco de dados

---
+++ src/database/fake_data.js
@@ -0,0 +1,37 @@
+//Dados
+const proffys = [
+  {
+    name: "Diego Fernandes",
+    avatar:
+      "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: 89998522,
+    bio: "Entusiasta das melhores tecnologias de química avançada. <br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Química",
+    cost: "20",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220],
+  },
+  {
+    name: "Felipe Alves",
+    avatar: "https://avatars.githubusercontent.com/u/104840530?v=4",
+    whatsapp: 47991209228,
+    bio: "A meta é alcançar estatisticas absurdas sobre o mundo DEV.",
+    subject: "Matemática",
+    cost: "500",
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220],
+  },
+  {
+    name: "Cristhyan Kohlhase",
+    avatar: "https://avatars.githubusercontent.com/u/25498050?v=4",
+    whatsapp: 6596008129,
+    bio: "FULL STACK.",
+    subject: "Matemática",
+    cost: "20000",
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220],
+  },
+];
